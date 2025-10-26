0dbea99cb7c728cf97619da41ead2cbb05869c83
Eduardo Müller Candaten <149803596+eduardocandaten@users.noreply.github.com>
2025-10-26 19:47:38 -0300

    feat: create `accounts` viewer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import json
+from django.http import HttpRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
+from .models import Account, Transaction
 
-# Create your views here.
+@csrf_exempt
+def accounts(request: HttpRequest):
+    match request.method:
+        case 'GET':
+            data = list(Account.objects.values())
+            return JsonResponse(data, safe=False)
+        
+        case 'POST':
+            body = json.loads(request.body.decode('utf-8'))
+            account = Account.objects.create(name=body['name'])
+            return JsonResponse({
+                'id': account.id,
+                'name': account.name,
+                'balance': account.balance
+            })
