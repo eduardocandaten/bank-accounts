087737581f589531e3da25ba2e3089c0f5717f78
Eduardo Müller Candaten <149803596+eduardocandaten@users.noreply.github.com>
2025-10-29 09:58:12 -0300

    test: create `POST` and `GET` tests in `/api/account`

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,35 @@
+import json
+from datetime import datetime
+from django.utils.dateparse import parse_datetime
 from django.test import TestCase
+from .models import Account
 
-# Create your tests here.
+class AccountViewTestCase(TestCase):
+    def test_post_accounts(self):
+        """POST /api/accounts with valid inputs"""
+        body = { 'name': 'Account 3' }
+        response = self.client.post(
+            '/api/accounts/',
+            data=json.dumps(body),
+            content_type='application/json'
+        )
+        data = json.loads(response.content.decode())
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(data, {
+            'id': data['id'],
+            'name': 'Account 3',
+            'balance': 0,
+            'created_at': data['created_at']
+        })
+        self.assertTrue(isinstance(data['id'], int))
+        self.assertTrue(isinstance(parse_datetime(data['created_at']), datetime))
+
+    def test_get_accounts(self):
+        """GET /api/accounts"""
+        response = self.client.get('/api/accounts/')
+        data = json.loads(response.content.decode())
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(isinstance(data, list))
+    
